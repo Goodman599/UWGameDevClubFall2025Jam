dde64b13a909a72861e31c0111e07a121b27869a
Goodman599 <Gonghm2007@gmail.com>
2025-10-11 15:55:57 -0700

    Got started on the ui

---
+++ scenes/ui_manager.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@onready var start_button : Button = $MainMenuRoot.start_buttuon
+@onready var UI_camera : Camera2D = $UICamera
+
+const SCREEN_POSITIONS = {
+	"start" : Vector2(0, 0),
+	"level_select" : Vector2(1920, 0),
+}
+const SCREEN_TRANSITION_TIME = 0.5
+const SCREEN_SIZE = Vector2(1920, 1080)
+
+
+func _ready() -> void:
+	start_button.pressed.connect(go_to_screen.bind(SCREEN_POSITIONS["level_select"]))
+	
+	
+	
+func go_to_screen(screen_position : Vector2):
+	var screen_tween = get_tree().root.create_tween()
+	screen_tween.tween_property(UI_camera, "position", screen_position + SCREEN_SIZE / 2, SCREEN_TRANSITION_TIME)
---
+++ scripts/main_menu.gd
@@ -0,0 +1,3 @@
+extends Control
+
+@export var start_buttuon : Button
