2f8c9215a87a0a61d09670636f764a645351d358
Goodman599 <Gonghm2007@gmail.com>
2025-10-17 16:27:29 -0700

    Tilemap prototype complete

--- scripts/level_tile_map.gd
+++ scripts/level_tile_map.gd
@@ -2,11 +2,25 @@ extends TileMapLayer
 
 
 enum CustomDataLayers {
+	IS_WALKABLE,
 	IS_WALL,
+	IS_DESTRUCTABLE,
 }
+
+func _ready():
+	destroy(Vector2i(1, 1))
 
 # Important in built functions:
 #	local_to_map() & map_to_local() converts local coordinates to/from cell coordinates
 
+# Given a tile position, and a property (Can be found using enumerator)
+# Returns the value for the custom data (transformative)
 func get_tile_property(tile_position: Vector2i, data_id : int):
 	return get_cell_tile_data(tile_position).get_custom_data_by_layer_id(data_id)
+
+
+# Given a tile position, and if it is breakable, turns it into a normal path.
+func destroy(tile_position : Vector2i):
+	if get_cell_tile_data(tile_position).get_custom_data_by_layer_id(CustomDataLayers.IS_DESTRUCTABLE):
+		# Change target tile into a path terrain
+		set_cells_terrain_connect([tile_position], 0, 0)
