6ba2bc5340f12a42523c6b35d9a2a0b4e1c6710d
ArmandMeyer <armand03@uw.edu>
2025-10-17 00:28:47 -0700

    finished basic crafting functionality

---
+++ scripts/resource_count.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@export var resource_id = -1
+
+func _ready() -> void:
+	updateResourceCounts()
+
+func updateResourceCounts():
+	var counts = get_parent().resources
+	for i in 5:
+		var label = get_node("Control/count_" + str(i))
+		label.text = str(counts[i])
