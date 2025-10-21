3f2296d2e9dc6cd62d6dc997cffb8744f17aa86a
ArmandMeyer <armand03@uw.edu>
2025-10-20 19:52:08 -0700

    Uncraftable potions will be inactive and grayed out

--- scripts/potion_logic.gd
+++ scripts/potion_logic.gd
@@ -3,17 +3,9 @@ extends Node2D
 var selected = false
 var mouse_offset = Vector2(0,0)
 var level_node : Node2D
-
-'''
-# gets the level_node from the ancestral family tree
-func _ready() -> void:
-	var gameplay_scene_node = get_parent().get_parent().get_parent()
-	print(gameplay_scene_node.name)
-	level_node = gameplay_scene_node.get_child(gameplay_scene_node.get_child_count() - 1)
-'''
+var level_started = false
 
 var crafting_costs = {
-	0 : [1, 0, 0, 0, 0],
 	1 : [0, 1, 0, 0, 0],
 	2 : [0, 0, 1, 0, 0],
 	3 : [0, 0, 0, 1, 0],
@@ -21,49 +13,71 @@ var crafting_costs = {
 }
 
 @export var id = -1
+# potion IDs:
+# 1 = destroy, 2 = freeze, 3 = mirror, 4 = speed
 
 @onready var starting_position = global_position
 @onready var area = $PotionHitbox
 
+
 func _process(delta):
-	if selected:
+	if selected and canCraft():
 		followMouse()
+	if level_started == false and level_node != null:
+		updateShader()
+		level_started = true
 		
 func followMouse():
 	global_position = get_global_mouse_position() + mouse_offset
 
 
 func _on_potion_hitbox_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and level_started:
 		if event.is_released():
 			selected = false
-			checkCouldronCollision()
+			checkCauldronCollision()
 			global_position = starting_position
 		else:
 			mouse_offset = global_position - get_global_mouse_position()
 			selected = true
 		
-func checkCouldronCollision():
+func checkCauldronCollision():
 	var overlapping = area.get_overlapping_areas()
 
 	for other in overlapping:
 		if other.name == "CauldronHitbox":
 			# crafting
 			if canCraft():
-				print("potion id " + str(id) + " used")
+				
+				craft()
+				#turn on grayscale for uncraftable potions
+				
+				return
 			
 func canCraft() -> bool:
 	if level_node == null:
 		print("No level selected!")
 		return false
-	print(level_node.name)
 	var remainingResources = level_node.resources
 	for i in len(remainingResources):
 		if remainingResources[i] < crafting_costs.get(id)[i]:
 			print("insufficient resources!")
 			return false
+	return true
+			
+func craft():	
+	var remainingResources = level_node.resources
 	for i in len(remainingResources):
 		remainingResources[i] -= crafting_costs.get(id)[i]
 		var counter = level_node.get_node("ResourceCount")
 		counter.updateResourceCounts()
-	return true
+	print("potion id " + str(id) + " used")
+	updateShader()
+		
+func updateShader():
+	print("updating shader for potion id: " + str(id))
+	if canCraft():
+		$PotionSprite.material.set_shader_parameter("active", false)
+	else:
+		$PotionSprite.material.set_shader_parameter("active", true)
+	
