904eb56210e671a6c662acc08ed7df3c48ead1ce
Goodman599 <Gonghm2007@gmail.com>
2025-10-21 17:33:22 -0700

    hmm...

--- scripts/level_select_root.gd
+++ scripts/level_select_root.gd
@@ -19,7 +19,8 @@ func _ready():
 			@warning_ignore("integer_division")
 			level_buttons.get_children()[i].pressed.connect(level_chosen.bind((i / 2 + 1)))
 			if i > max_completed_level:
-				level_buttons.get_children()[i].hide()
+				pass
+				#level_buttons.get_children()[i].hide()
 
 func _process(_delta):
 	if Input.is_action_just_pressed("reset"):
